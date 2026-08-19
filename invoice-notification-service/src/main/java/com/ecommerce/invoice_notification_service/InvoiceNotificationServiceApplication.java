4d81fbbfdf0922c60bddac4ffd4038c04d875912
Kunal Deshmukh <kddeshmukh932@gmail.com>
2026-08-19 13:02:45 +0530

    Initial microservices project setup

---
+++ invoice-notification-service/src/main/java/com/ecommerce/invoice_notification_service/InvoiceNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.invoice_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ invoice-notification-service/src/test/java/com/ecommerce/invoice_notification_service/InvoiceNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.invoice_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
